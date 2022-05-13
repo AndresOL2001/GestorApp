8b0345491226fe98acddf00d61eb9de337573c39
Andres <andresolaje@gmail.com>
2022-05-12 20:04:41 -0700

    second commit

--- src/app/pages/estatus/estatus.component.ts
+++ src/app/pages/estatus/estatus.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-estatus',
   templateUrl: './estatus.component.html',
   styleUrls: ['./estatus.component.css']
 })
 export class EstatusComponent implements OnInit {
-
+bandera = false;
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
+  abrir(){
+  }
 }
